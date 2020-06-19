a7560f4727dbfc22dc92f017808f7eb5c2945a29
8BitNrd <54446016+8bitNrd@users.noreply.github.com>
2020-06-19 17:29:18 -0500

    Update Deploy-OrganizationUsers.ps1
    Updated misspelled variable and updated $domainName and $topLevelDomain to be auto assigned by using $env:USERDNSDOMAIN

--- Deploy-OrganizationUsers.ps1
+++ Deploy-OrganizationUsers.ps1
@@ -1,16 +1,19 @@
-$domainName = "lab"
-$topLevelDomain = "lcl"
+
 $CEODepartment = "Administration"
 $ManagersPerDepartment = 2
 $EmployeesPerManager = 10
 $departmentList = "Customer Service", "Sales", "Marketing", "Legal", "Help Desk", "Human Resources", "Accounting"
 
+#Domain
+$domainName =  ($env:USERDNSDOMAIN).Split(".")[0]
+$topLevelDomain = ($env:USERDNSDOMAIN).Split(".")[1]
+
 #Functions
-Function New-FirstName 
+Function Renadom-FirstName 
 {
    Return "Erich", "Domenic", "Wayne", "Darryl", "Luis", "Darell", "King", "Salvatore", "George", "Francis", "Sherwood", "Dino", "Patrick", "Burton", "Columbus", "Isaias", "Shad", "Todd", "Trent", "Garfield", "Frankie", "Lenny", "Scottie", "Romeo", "Jame", "Neville", "Terrance", "Clay", "Ulysses", "Gonzalo", "Fidel", "Virgil", "Bruno", "Rashad", "Archie", "Jeramy", "Cleo", "Yong", "Judson", "Earle", "Mack", "Tyrell", "Robbie", "Fermin", "Jamey", "Santos", "Pedro", "Albert", "Jerrold", "Arnulfo", "Ammie", "Susy", "Renay", "Terica", "Dalia", "Noemi", "Avelina", "Janelle", "Le", "Marquitta", "Kathe", "Lavada", "Lorine", "Delila", "Olga", "Lecia", "Dovie", "Theola", "Deann", "Donetta", "Elicia", "Vella", "Valery", "Verlene", "Cierra", "Kizzy", "Kerri", "Laurine", "Junie", "Lavonda", "Chieko", "Carissa", "Stefania", "Gina", "Neda", "Sunny", "Corazon", "Marylouise", "Anitra", "Elfriede", "Lillie", "Catalina", "Caron", "Pinkie", "Shanel", "Rachel", "Fay", "Matilde", "Renae", "Tish" | Get-Random
 }
-Function New-LastName 
+Function Renadom-LastName 
 {
    return "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "Hernandez", "King", "Wright", "Lopez", "Hill", "Scott", "Green", "Adams", "Baker", "Gonzalez", "Nelson", "Carter", "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sanchez", "Morris", "Rogers", "Reed", "Cook", "Morgan", "Bell", "Murphy", "Bailey", "Rivera", "Cooper", "Richardson", "Cox", "Howard", "Ward", "Torres", "Peterson", "Gray", "Ramirez", "James", "Watson", "Brooks", "Kelly", "Sanders", "Price", "Bennett", "Wood", "Barnes", "Ross", "Henderson", "Coleman", "Jenkins", "Perry", "Powell", "Long", "Patterson", "Hughes", "Flores", "Washington", "Butler", "Simmons", "Foster", "Gonzales", "Bryant", "Alexander", "Russell", "Griffin", "Diaz", "Hayes" | Get-Random
 }
@@ -56,8 +59,8 @@ function New-Employee
       Password = New-RandomPassword
       Phone = New-FakePhoneNumber
       EmployeeNumber = New-EmployeeNumber 
-      GivenName = New-FirstName
-      Surname = New-LastName
+      GivenName = Renadom-FirstName
+      Surname = Renadom-LastName
    }
 
    [Array]$userExist = Get-ADUser -Filter "Surname -like '$($EmployeeData.surName)*'" -Properties GivenName,Surname | Where-Object {$_.GivenName -eq $EmployeeData.GivenName}
@@ -69,7 +72,7 @@ function New-Employee
       $EmployeeData.Surname = ($EmployeeData.Surname + ($int = $int + 1))
    }
    [String]$SamAccountName = "$($EmployeeData.GivenName.Substring(0)[0])" + "$($EmployeeData.surName)"
-   return New-ADUser -Name ($EmployeeData.GivenName+" "+$EmployeeData.surName)-GivenName $EmployeeData.GivenName -Surname $EmployeeData.surName -AccountPassword (ConvertTo-SecureString $EmployeeData.password -AsPlainText -Force) -OfficePhone $phone -Enabled $true -Department $Department -SamAccountName $SamAccountName -EmployeeNumber $EmployeeNumber -DisplayName ($EmployeeData.GivenName+" "+$EmployeeData.surName) -EmailAddress ($EmployeeData.GivenName+"."+$EmployeeData.surName+"@"+$domainName+".$topLevelDomai") -Path $Path -PassThru
+   return New-ADUser -Name ($EmployeeData.GivenName+" "+$EmployeeData.surName)-GivenName $EmployeeData.GivenName -Surname $EmployeeData.surName -AccountPassword (ConvertTo-SecureString $EmployeeData.password -AsPlainText -Force) -OfficePhone $phone -Enabled $true -Department $Department -SamAccountName $SamAccountName -EmployeeNumber $EmployeeNumber -DisplayName ($EmployeeData.GivenName+" "+$EmployeeData.surName) -EmailAddress ($EmployeeData.GivenName+"."+$EmployeeData.surName+"@"+$domainName+".$topLevelDomain") -Path $Path -PassThru
 }
 
 #create orgranization unit to store users
@@ -94,8 +97,8 @@ Foreach ($deparment in $departmentList)
    New-ADOrganizationalUnit -Name "$deparment" -Path "$ouUserPath" -ProtectedFromAccidentalDeletion $False
    
    #Creating Managers
-   [Int]$ManCount = 0
-   While ([Int]$ManCount -lt $ManagersPerDepartment)
+   [Int]$managerCount = 0
+   While ([Int]$managerCount -lt $ManagersPerDepartment)
    {
       $Manager = New-Employee -Department $deparment -Path $("OU=$deparment," + $ouUserPath)
       Set-ADUser -Identity $Manager -Manager $VP
@@ -110,6 +113,6 @@ Foreach ($deparment in $departmentList)
          Write-Output "Created Employee $($Employee.name)"
          $EmpCount++
       }
-      $ManCount++
+      $managerCount++
    }
 }
